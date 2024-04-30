d0d79acca6004e89b0185fe17a23a67303c5009c
Marina.P <marina.p.fullstack@gmail.com>
2024-04-30 19:53:39 +0200

    Добавление файлов по модулю D3

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+def index(request):
+    latest_articles = Article.objects.order_by('-pub_date')[:10]
+    return render(request, 'news/index.html', {'latest_articles': latest_articles})
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'news/detail.html', {'article': article})
